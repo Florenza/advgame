e0649cd8998d855611109401db34f9377202a2ed
DESKTOP-1KM32GD\edz <773815862@qq.com>
2018-06-04 17:57:39 +0800

    6.4

---
+++ index.js
@@ -0,0 +1,5 @@
+define(function(require,exports,module){
+    var jQuery = require('jquery');// require前锋上前听令
+    var mysea2 = require('mysea2');// mysea2小将军上前听令
+    console.log(mysea2.show()+'jquery version '+$().jquery);
+});
---
+++ public.js
@@ -0,0 +1,9 @@
+var publicParameter = {
+    gname: 0,                   //游戏类型：  0 大转盘，1 翻红包，2 九宫格，3 砸金蛋，4划红包 ...
+    playnum: 8,                 //游戏的次数
+    gameCookies: "roteGame",    //游戏Cookie名字
+    gameStartPoiton: 0,         //游戏起始位置   （如有必要）
+    gameEndPosition: 4,         //游戏结束位置   （如有必要）
+    gameSpeed: 6,               //游戏速度       （如有必要）
+    modalType:0                 //弹窗样式的选择
+}
